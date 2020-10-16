dab87780127848ead9bf5ab63abd2f1208814093
Alessandro Ercolani <ale.ercolani@gmail.com>
2020-10-16 14:52:57 +0200

    [#175227424] chenge dashboard json data from azure to s3 porting from databricks to pdnd

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -3,7 +3,7 @@ function loadJSON(callback) {
     xobj.overrideMimeType("application/json");
     xobj.open(
       "GET",
-      "https://publicdashboard.blob.core.windows.net/public/dashboard-data.json",
+      "https://pdnd-prod-dl-1-public-data.s3.eu-central-1.amazonaws.com/dashboard/pagopa/dashboard-data.json",
       true
     ); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
