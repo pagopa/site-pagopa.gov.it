ff48d2548af12db5ac544fb874687e9204c73b28
Alessandro Ercolani <ale.ercolani@gmail.com>
2021-01-19 17:37:44 +0100

    Minor fix dashboard pagopa

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -275,7 +275,7 @@ function loadJSON(callback) {
   
     function sixMonthPredictionTotal(forecastByMonth) {
       return forecastByMonth
-        .map((element) => Math.round(element.yhat))
+        .map((element) => Math.round(element.yhat_upper))
         .reduce((a, b) => a + b, 0);
       // .toLocaleString();
     }
