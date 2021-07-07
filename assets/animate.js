cfca60c3698f43ddd575873000741f776d4548a5
Vito Falco <vitofalco@gmail.com>
2021-07-07 10:15:55 +0200

    fallback for IntersectionObserver

--- assets/animate.js
+++ assets/animate.js
@@ -4,8 +4,16 @@ function callback(entries) {
    target.classList.add('fade-in-bottom');
 }
 
-  
-let observer = new IntersectionObserver(callback);
-animateEls.forEach( el => {
-   observer.observe(el); 
-});
+if (!('IntersectionObserver' in window) ||
+    !('IntersectionObserverEntry' in window) ||
+    !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
+   animateEls.forEach( el => {
+      el.classList.add('fade-in-bottom');
+   });
+} else {
+   let observer = new IntersectionObserver(callback);
+   animateEls.forEach( el => {
+      observer.observe(el); 
+   });
+
+}
