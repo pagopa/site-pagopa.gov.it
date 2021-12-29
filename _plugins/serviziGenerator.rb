1b34d390fa6b6fb2a212afa315cd0b6fa2f2eb56
Infectedmachine <nazcheck@gmail.com>
2021-12-29 11:46:52 +0100

    Add convenzioni

--- _plugins/serviziGenerator.rb
+++ _plugins/serviziGenerator.rb
@@ -32,6 +32,7 @@ Jekyll::Hooks.register :site, :after_init do |doc, payload|
     topass['lang'] = 'it'
     topass['child_of_ref'] = 'prestatori-servizi-di-pagamento-elenco-psp-attivi'
     topass['omit_pagehead'] = true
+    topass['url_informazioni_psp'] = psp['url_informazioni_psp']
     topass['services'] = psp['content']
 
     File.open(dir+name+".md", "w") { |file| file.write(topass.to_yaml + '---' + '
