06ac3936f2339fc319eb7b368b25750d76578b19
Vito Falco <vitofalco@gmail.com>
2022-01-13 18:34:47 +0100

    Refact. Better management of detail page. Restructred after a change of base data

--- _plugins/serviziGenerator.rb
+++ _plugins/serviziGenerator.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'json'
 require 'down'
+require 'yaml'
 
 isDevMode = ENV['JEKYLL_ENV']=='production' ? false : true
 downloadUrl = "https://pdnd-prod-dl-1-public-data.s3.eu-central-1.amazonaws.com/dashboard/pagopa/output_elenco_servizi.json"
@@ -13,72 +14,55 @@ rescue
     raise "File unreachable"
 end
 
+def groupServices(services, pspConfig)
+  services_grouped = {}
+  pspConfig.each do |pspItem, pspItemValue|
+    services_perItem = []
+    pspItemValue['condition'].each do |condition|
+      services_selected_by_condition = services.select{|item| item[condition]==true and item['canale_mod_pag_code'] < 3 }
+      if services_selected_by_condition.size > 0
+        services_perItem.concat services_selected_by_condition
+      end
+    end
+    next if services_perItem.size < 1
+    services_grouped[pspItem] = services_perItem.size == 0 ? [] : services_perItem.group_by{|h| h[pspItemValue['group_by']] }
+  end
+
+  return services_grouped
+end
+
 Jekyll::Hooks.register :site, :after_init do |site|
 
   pspdir = "_pspservizi"
   jsondir = "assets/jsonpsp"
+  pspConfig = YAML.load_file('_data/psp-metodi.yml')
 
   data_hash = JSON.parse(fileitem.read)
   # PSP listed in a graph
-  psp_dict = {}
+  psp_dict = data_hash['content'].group_by{|h| h['codice_abi']}
   # Stuffs arranged by tipo_vers_cod
   psp_by_method = {}
 
-  data_hash['content'].each_with_index do |item, index|
-    # Let's create a dict of PSP to arrange data by a graph instead a flat structure
-    next if item['is_duplicated'] == true || item['codice_abi'] == nil
-
-    # Useful to generate a single page PSP
-    if psp_dict.key?(item['codice_abi'])
-      psp_dict[item['codice_abi']].push(item)
-    else
-      psp_dict[item['codice_abi']] = []
-      psp_dict[item['codice_abi']].push(item)
-    end
-
-    # Useful to generate N json for N pay methods (aka tipo_vers_cod)
-    if psp_by_method.key?(item['tipo_vers_cod'])
-      psp_by_method[item['tipo_vers_cod']].push(item)
-    else
-      psp_by_method[item['tipo_vers_cod']] = []
-      psp_by_method[item['tipo_vers_cod']].push(item)
-    end
-
-    # If in DEV mode we can skip a lot of computation...
-    if isDevMode && psp_dict.size > 20
-      puts "+++++ DEV MODE: only 20 PSP catched +++++++"
-      break
-    end
-  end
-
-  # Create a new md file for every psp
-  psp_dict.each do |key, value|
-    # every item of the hash as a specific set of info suplicated, so we can use the first one
+  psp_dict.each_with_index do |(key, value), index|
+    # every item of the hash as a specific set of info duplicated, so we can use the first one
     element = value[0]
     name = element['codice_abi']
     front_matter = Hash.new
-    front_matter['layout'] = 'page'
+    front_matter['layout'] = 'psp'
     front_matter['ref'] = element['codice_abi']
     front_matter['title'] = element['psp_rag_soc']
     front_matter['lang'] = 'it'
     front_matter['child_of_ref'] = 'prestatori-servizi-di-pagamento-elenco-psp-attivi'
     front_matter['omit_pagehead'] = true
     front_matter['url_informazioni_psp'] = element['url_informazioni_psp']
-    front_matter['services'] = value
+    front_matter['services'] = groupServices(value, pspConfig)
+    next if front_matter['services'].empty? or name==''
+
     # Let's create a markdown page
     File.open(pspdir + '/' + name.to_s + ".md", "w") { |file| file.write(
       front_matter.to_yaml + '---' + '
-      {% include psp-servizi.html %}
       '
     ) }
   end
-
-  # Create a json file for every "payment method"
-  psp_by_method.each do |key, value|
-    File.write(jsondir + '/' + key.downcase + '.json', JSON.dump(value))
-  end
-
-  # Create a yml with payment methods (useful for "Confronta tariffe")
-  File.write('_data/psp-metodi.yml', psp_by_method.keys.to_yaml )
 
 end
