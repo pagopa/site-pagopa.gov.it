f6d1d137a273411b048513d76b95b3265bddcc93
Vito Falco <vitofalco@gmail.com>
2022-02-07 10:33:24 +0100

    Onetrust setup

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,12 +1,30 @@
-if (document.cookie.indexOf('cookies_consent=true') === -1) {
-    window['ga-disable-UA-139250396-1'] = true;
+var targCookiesGroup = "C0002";
+var checkValue = targCookiesGroup + '%3A1';
+var OTCookieValue = document.cookie;
+
+function ga_init() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-139250396-1', 'auto');
+    ga('set', 'anonymizeIp', true);
+    ga('send', 'pageview');
 }
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+function OptanonWrapper() {
+    /* wrapping in OneTrust callback function */
 
-ga('create', 'UA-139250396-1', 'auto');
-ga('set', 'anonymizeIp', true);
-ga('send', 'pageview');
+    OneTrust.OnConsentChanged(function () {
+        var activeGroups = OnetrustActiveGroups;
+        if (activeGroups.indexOf(targCookiesGroup) > -1) {
+            ga_init();
+        }
+      });
+
+}
+
+if (OTCookieValue.indexOf(checkValue) > -1) {
+    ga_init();
+}
--- assets/main.js
+++ assets/main.js
@@ -115,25 +115,6 @@ $(function() {
         
     });
 
-    $('.cookiebar__close').on('click', function(e) {
-        var $cookiebar = $('.cookiebar');
-        var date = new Date();
-        date.setTime(date.getTime() + (7*24*60*60*1000));
-        //var expires = "expires=" + date.toUTCString();
-        var expires = "expires=0";
-
-        document.cookie = "cookies_notnow=true;" + expires + "; path=/";
-
-        $cookiebar.removeClass('show').addClass('notnow').attr('aria-hidden','true');
-
-    });
-
-// test if user says no-cookies-now
-    if (!(document.cookie.indexOf('cookies_consent=true') === -1 && document.cookie.indexOf('cookies_notnow=true') === -1 )) {
-        var $cookiebar = $('.cookiebar');
-        $cookiebar.removeClass('show').addClass('notnow').attr('aria-hidden','true');
-        
-    }
 
     function getHash() {
         var hash = window.location.hash;
