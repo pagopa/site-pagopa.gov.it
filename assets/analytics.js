d6e638e96082031d0648333fea78cf0283f4d11a
Vito Falco <vitofalco@gmail.com>
2023-06-13 14:15:45 +0200

    [WEB-231] Add local matomo, and dismiss google analytics (#533)
    * Add local matomo.
    Dismiss g analytics
    
    * Drop old cookies

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,50 +1,3 @@
-var targCookiesGroup = "C0002";
-var checkValue = targCookiesGroup + "%3A1";
-var OTCookieValue = document.cookie;
-
-function ga_init() {
-  (function (i, s, o, g, r, a, m) {
-    i["GoogleAnalyticsObject"] = r;
-    (i[r] =
-      i[r] ||
-      function () {
-        (i[r].q = i[r].q || []).push(arguments);
-      }),
-      (i[r].l = 1 * new Date());
-    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(
-    window,
-    document,
-    "script",
-    "https://www.google-analytics.com/analytics.js",
-    "ga"
-  );
-
-  ga("create", "UA-139250396-1", "auto");
-  ga("set", "anonymizeIp", true);
-  ga("send", "pageview");
-}
-
-function OptanonWrapper() {
-  /* wrapping in OneTrust callback function */
-
-  OneTrust.OnConsentChanged(function () {
-    var activeGroups = OnetrustActiveGroups;
-    if (activeGroups.indexOf(targCookiesGroup) > -1) {
-      ga_init();
-    }
-  });
-}
-
-if (OTCookieValue.indexOf(checkValue) > -1) {
-  ga_init();
-}
-
-/* === MATOMO ==*/
-
 var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(["trackPageView"]);
@@ -57,6 +10,15 @@ _paq.push(["enableLinkTracking"]);
     g = d.createElement("script"),
     s = d.getElementsByTagName("script")[0];
   g.async = true;
-  g.src = "//cdn.matomo.cloud/pagopa.matomo.cloud/matomo.js";
+  g.src = "/assets/matomo.js";
   s.parentNode.insertBefore(g, s);
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  //drop old GA cookies
+  var cookies = Cookies.get();
+  var gaCookies = Object.keys(cookies).filter((k) => k.startsWith("_g"));
+  for (var gc in gaCookies) {
+    Cookies.remove(gaCookies[gc], { path: "", domain: ".pagopa.gov.it" });
+  }
+});
