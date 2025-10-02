ea4464a1d876bac935f059a19d0573e110299d92
Stefano Frontini <stefanofrontini75@gmail.com>
2025-10-02 09:12:09 +0200

    fix trx-amount ordering

--- assets/dashboard/_next/static/chunks/pages/_error.js
+++ assets/dashboard/_next/static/chunks/pages/_error.js
@@ -1,1 +1,1 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{2164:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(9341)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,792],()=>n(2164)),_N_E=_.O()}]);
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{1981:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(8529)}])}},function(n){n.O(0,[888,179],function(){return n(n.s=1981)}),_N_E=n.O()}]);
